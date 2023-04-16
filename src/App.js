3d249a1338cebd0d3ba8e7c4dbcb02503362a227
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-04-16 11:29:59 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 const App = () => {
   return (
     <div className="wrapper clear">
+      <div className="overlay ">
+        <div className="drawer d-flex flex-column">
+          <h2 className="mb-10">Корзина</h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center">
+              <div
+                style={{ backgroundImage: "url(/img/1.png)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="description mr-20">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeButton"
+                width={30}
+                height={30}
+                src="/img/ButtonRemove.png"
+                alt="remove"
+              />
+            </div>
+            <div className="cartItem d-flex align-center">
+              <div
+                style={{ backgroundImage: "url(/img/2.png)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="description mr-20">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeButton"
+                width={30}
+                height={30}
+                src="/img/ButtonRemove.png"
+                alt="remove"
+              />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button>Оформить заказ</button>
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
+        <div className="d-flex ">
           <img
-            className="mr-15"
+            className="mr-15 mt-10"
             width={40}
             height={40}
             src="/img/logo.png"
@@ -12,7 +69,7 @@ const App = () => {
           />
           <div>
             <h3 className="text-uppercase">REACT SNEAKERS</h3>
-            <p className="opacity-5"> Магазин лучших кроссовок</p>
+            <p className="opacity-5 "> Магазин лучших кроссовок</p>
           </div>
         </div>
         <ul className="d-flex">
@@ -26,10 +83,19 @@ const App = () => {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex  align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-bLock   d-flex">
+            <img width={40} height={40} src="/img/search.jpg" alt="Search" />
+            <input placeholder="Поиск ..." />
+          </div>
+        </div>
 
         <div className="d-flex">
           <div className="card">
+            <div className="favorite">
+              <img width={40} height={40} src="/img/unlike.png" alt="unlike" />
+            </div>
             <img width={133} height={112} src="/img/1.png" alt="Cart" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between">
